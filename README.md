f27f0d3ab4844ba52f6bcd53750ebc74cc84316d
zong4 <zzoonng@163.com>
2025-02-07 10:17:32 +0800

    add parser

--- README.md
+++ README.md
@@ -4,32 +4,32 @@ Scan the description http://zzoonng.top/2025/01/29/AI%E5%AD%A6%E4%B9%A000/.
 
 ## Search
 
-### Maze
-
-### Tic-Tac-Toe
+1. Maze
+2. Tic-Tac-Toe
 
 ## Knowledge
 
-### Logic
-
-### Mine Sweeper
-
-### Knight Or Knave
-
-### 狼人杀
+1. Logic
+2. Mine Sweeper
+3. Knight Or Knave
+4. 狼人杀
 
 ## Uncertainty
 
-### Page Rank
-
-### Heredity
-
-### Sherlock Stories
+1. Page Rank
+2. Heredity
+3. Sherlock Stories
 
 ## Optimization
 
-### Cross Word
+1. Cross Word
 
 ## Learning
 
-### Shopping
+1. Shopping
+
+## Neural Networks
+
+1. Traffic
+
+## Language
---
+++ parser/parser.py
@@ -0,0 +1,92 @@
+import nltk
+nltk.download('punkt_tab')
+
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP | S ConjP S | S PP S
+NP -> N | Det NP | AdjP NP
+VP -> V | Adv VP | VP Adv | VP PP | VP NP | VP Conj VP
+ConjP -> Conj | Conj NP | Conj VP
+PP -> P NP | P S
+AdjP -> Adj | Adj AdjP
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def preprocess(sentence):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    words = nltk.word_tokenize(sentence)
+    print(words)
+    words = [word.lower() for word in words if any(char.isalpha() for char in word)]
+    return words
+
+
+def np_chunk(tree):
+    """
+    Return a list of all noun phrase chunks in the sentence tree.
+    A noun phrase chunk is defined as any subtree of the sentence
+    whose label is "NP" that does not itself contain any other
+    noun phrases as subtrees.
+    """
+    chunks = []
+    for subtree in tree.subtrees():
+        if subtree.label() == "N":
+            chunks.append(subtree)
+    return chunks
+
+
+main()
